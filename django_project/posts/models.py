536dd7146c356cd289bff15241294394cb572255
strikeouts27 <stribling.andrew@gmail.com>
2023-07-20 21:21:19 -0500

    New updates for Fly deployment

--- django_project/posts/models.py
+++ django_project/posts/models.py
@@ -1,12 +1,9 @@
-from django.db import models 
+from django.db import models
+
 
 # Create your models here.
-class Post(models.Model): # new
+class Post(models.Model):  # new
     text = models.TextField()
-from django.db import models 
 
-class Post(models.Model):
-     text = models.TextField()
-
-     def __str__(self):
+    def __str__(self):
         return self.text[:50]
